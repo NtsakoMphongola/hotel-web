12ff7e6bef5513eb1cfaefe5077f4ecb6dab610d
NtsakoMphongola <105928245+NtsakoMphongola@users.noreply.github.com>
2022-07-12 15:25:48 +0200

    init

---
+++ src/components/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function Homepage() {
+  return (
+    <>
+    <section>
+        <div className='container flxe1'>
+            <div className='navbar'>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Bookings</li>
+                    <li>Login</li>
+                    <li>Register</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <h1>Mphongola Hotel</h1>
+    </>
+  )
+}
+
+export default Homepage;
