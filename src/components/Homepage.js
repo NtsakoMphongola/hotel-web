d3671c6f4bda6c8e6241c410062faf90cdd703bc
NtsakoMphongola <105928245+NtsakoMphongola@users.noreply.github.com>
2022-07-12 15:28:58 +0200

    changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import homelogo from '../assets/Block6.jpg';
+import 
 
 
 function Homepage() {
   return (
     <>
+    
     <section>
         <div className='container flxe1'>
+            <img src={homelogo} className="homelog" />
             <div className='navbar'>
-                <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                    <li>Bookings</li>
-                    <li>Login</li>
-                    <li>Register</li>
-                </ul>
+            <ul class="nav-menu">
+            <li> <a href="#home">Home</a> </li>
+            <li> <a href="#about">about</a> </li>
+            <li> <a href="#room">room</a> </li>
+            <li> <a href="#services">services</a> </li>
+            <li> <a href="#shop">shop</a> </li>
+            <li> <a href="#testimonials">testimonials</a> </li>
+            <li> <a href="#gallary">gallary</a> </li>
+            <li> <a href="#blog">blog</a> </li>
+            <li> <a href="#contact">contact</a> </li>
+          </ul>
             </div>
         </div>
     </section>
